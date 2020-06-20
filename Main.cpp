b54142d79a929e69d43507a5c07de20df21d6ad0
RichFox1990 <richardalanfox1990@gmail.com>
2020-06-20 23:50:48 +0100

    added Remove, length + template for class + console output - UPDATE 1

--- Main.cpp
+++ Main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
+#include <string>
+#include <stdio.h>
 
-struct DataNode
+template <typename T> struct DataNode
 {
-	int data;
-	DataNode* next;
+	T data;
+	DataNode<T>* next;
 };
 
-class LinkedList
+template <typename T> class LinkedList
 {
 private:
-	DataNode* head;
+	DataNode<T>* head, * tail;
+	DataNode<T>* newData = nullptr; 
+	int lengthOfList = 0;
+
 public:
 
 	LinkedList()
 	{
-		head = nullptr;
+		head = tail = newData = nullptr;
 	}
 
 	// Methods
-	void Add(int value)
+	void Add(T value)
 	{
-		DataNode* newData = new DataNode;
+		newData = new DataNode<T>;
 
 		newData->data = value;
 		newData->next = nullptr;
@@ -28,41 +33,123 @@ public:
 		if (head == nullptr)
 		{
 			head = newData;
+			tail = newData;
+			lengthOfList++;
 			return;
 		}
 
 		else
 		{
-			DataNode* tempHead = head;
-
-			while (tempHead->next != nullptr)
-				tempHead = tempHead->next;
-
-			tempHead->next = newData;
+			tail->next = newData;
+			tail = tail->next;
+			lengthOfList++;
 		}
+	}
+
+	int length(bool print = false)
+	{
+		if (print)
+			std::cout << "There are " << lengthOfList << " elements in the list." << std::endl; // 
+
+		return lengthOfList;
 	}
 
 	void Print()
 	{
-		DataNode* tempHead = head;
-		
-		if (tempHead != nullptr)
+		if (head == nullptr) // if list is empty
 		{
-			std::cout << tempHead->data << std::endl;
+			std::cout << "ERROR: There is no data in the list to print." << std::endl;
+			return;
+		}
+		else
+		{
+			DataNode<T>* tempHead = head;
 
 			while (tempHead->next != nullptr)
 			{
+				std::cout << tempHead->data << ", ";
 				tempHead = tempHead->next;
-				std::cout << tempHead->data << std::endl;
 			}
+			std::cout << tempHead->data << std::endl;
 		}
 	}
 
-	void Remove()
+	void TailPrint()
+	{
+		if (tail != nullptr)
+			std::cout << "Last value in the list is: " << tail->data << std::endl;
+		else
+			std::cout << "ERROR: The list is empty" << std::endl;
+	}
+
+	void Remove(int elementPosition) // 1 -> ... lengthOfList.
+	{
+		if (head == nullptr)
+		{
+			std::cout << "ERROR: The list has no data to delete" << lengthOfList << std::endl;
+			return;
+		}
+
+		else if ( (elementPosition > lengthOfList) || (elementPosition < 1) )
+		{
+			std::cout << "ERROR: You requested an number outside the range of the list. The size of the list is: " << lengthOfList << std::endl;
+			return;
+		}
+
+		else if (elementPosition == 1)
+		{
+			if (lengthOfList > 1)
+			{
+				DataNode<T>* toDelete = head;
+				head = head->next;
+
+				delete toDelete;
+				std::cout << "Removed the 1st element from the list" << std::endl;
+				lengthOfList--;
+				return;
+			}
+			else
+			{
+				delete head;
+				std::cout << "Removed the 1st element from the list. The list is now empty" << std::endl;
+				head = tail = nullptr;
+				lengthOfList--;
+			}
+		}
+
+		else
+		{
+
+			int currentElement = 2;
+
+			DataNode<T>* previous = head;
+
+			while (currentElement != elementPosition)
+			{
+				previous = previous->next;
+				currentElement++;
+			}
+
+			DataNode<T>* toDelete = previous->next;
+
+			previous->next = toDelete->next;
+
+			if (previous->next == nullptr) // if we are deleting the last Datanode - set tail to the previous node)
+			{
+				tail = previous;
+			}
+
+			delete toDelete;
+			std::cout << "Removed the element in position " << currentElement << " from the list." << std::endl;
+			lengthOfList--;
+
+		}
+	}
+
+	void Pop()
 	{
 
 	}
-
 
 };
 
@@ -73,15 +160,31 @@ public:
 int main()
 {
 
-	LinkedList myList;
+	LinkedList<std::string> myList;
 	
-	myList.Add(1);
-	myList.Add(2);
-	myList.Add(3);
-	myList.Add(4);
-	myList.Add(5);
+	std::string text = "hello";
+
+	myList.Add("testing");
+	myList.Add("1");
+	myList.Add("2");
 
 	myList.Print();
+	myList.length(true);
+
+	myList.Remove(3);
+	myList.Print();
+	myList.length(true);
+
+	myList.TailPrint();
+
+
+	myList.Remove(2);
+	myList.TailPrint();
+	myList.Remove(1);
+	myList.TailPrint();
+	myList.Print();
+	myList.length(true);
+
 
 	int freeze = 0;
 	std::cin >> freeze;
---
+++ README.md
@@ -0,0 +1,15 @@
+# C++ Linked List
+
+### Update 1
+- Added tracking of last dataNode (tail) for constant time Addition of nodes.
+- Added tailPrint function to print the last value in the list.
+- Made dataNode and class a template. Any data type can be used and compiled at runtime.
+  Class passes type to the datanode when creating new nodes for the list.
+- Added a remove function to delete a node by passing an int argument to the funtion.
+- Added a length function to return and print the length/size of the list.
+- Added cout messages for ease of testing and visual feedback.
+
+### Initial Commit
+- Basic implimentation of Data struct
+- Class for linked list
+- Basic method for Add, Print.
